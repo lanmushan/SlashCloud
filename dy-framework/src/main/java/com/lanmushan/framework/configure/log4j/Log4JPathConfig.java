dbbe3515e6ff5908792f3a04fca99486b42aab70
大熊猫 <lanmu_shan@163.com>
2020-06-09 21:28:30 +0800

    正确配置

---
+++ dy-framework/src/main/java/com/lanmushan/framework/configure/log4j/Log4JPathConfig.java
@@ -0,0 +1,16 @@
+package com.lanmushan.framework.configure.log4j;
+
+import java.io.File;
+
+/**
+ * @Author dy
+ * @Date 2020/6/7 21:07
+ * @Version 1.0
+ */
+
+public class Log4JPathConfig {
+    static {
+        File file=new File("");
+     //   PropertyConfigurator.configure(file.getAbsolutePath()+"//resource//config//log4j.properties");
+    }
+}
