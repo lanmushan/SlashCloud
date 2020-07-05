ace14deb9edc10f694c1b87ff0e96b8a68b7c7bd
cayden <chengliang@leyongleshi.com>
2020-07-06 00:42:19 +0800

    扫描所有接口，并且返回注释信息:注意,包名必须以com.langmushan开始，结尾必须是controller

---
+++ dy-framework/src/main/java/com/lanmushan/framework/entity/DaiYuApiInfo.java
@@ -0,0 +1,18 @@
+package com.lanmushan.framework.entity;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author cayden
+ * @date 2020/7/6 0:11
+ */
+@Data
+public class DaiYuApiInfo {
+
+    @ApiModelProperty("api名称")
+    private String apiName;
+
+    @ApiModelProperty("api链接")
+    private String apiUrl;
+}
