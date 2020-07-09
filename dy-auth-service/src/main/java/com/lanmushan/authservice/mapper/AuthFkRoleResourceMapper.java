8412b2dbe68f3c70edaa27c931a4653b47bcf347
大熊猫 <lanmu_shan@163.com>
2020-07-09 23:12:04 +0800

    更新数据版本

--- dy-auth-service/src/main/java/com/lanmushan/authservice/mapper/AuthFkRoleResourceMapper.java
+++ dy-auth-service/src/main/java/com/lanmushan/authservice/mapper/AuthFkRoleResourceMapper.java
@@ -1,5 +1,6 @@
 package com.lanmushan.authservice.mapper;
 
+import com.lanmushan.framework.dto.QueryInfo;
 import com.lanmushan.framework.mapper.QueryMapper;
 import tk.mybatis.mapper.additional.idlist.IdListMapper;
 import tk.mybatis.mapper.common.special.InsertListMapper;
@@ -9,6 +10,7 @@ import com.lanmushan.authservice.entity.AuthFkRoleResource;
 /**
  * (AuthFkRoleResource)表数据库访问层
  *
+ * @see com.lanmushan.framework.mapper.QueryMapper#selectQueryById(QueryInfo)
  * @author daiyu
  * @since 2020-06-15 22:13:47
  */
