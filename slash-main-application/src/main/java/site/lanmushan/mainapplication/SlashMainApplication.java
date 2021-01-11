8a79fce39e7d823c30784a0ad6de9f1958e681c5
大熊猫 <lanmu_shan@163.com>
2021-01-11 23:18:42 +0800

    重新分包

---
+++ slash-main-application/src/main/java/site/lanmushan/mainapplication/SlashMainApplication.java
@@ -0,0 +1,13 @@
+package site.lanmushan.mainapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlashMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlashMainApplication.class, args);
+    }
+
+}
---
+++ slash-main-application/src/test/java/site/lanmushan/mainapplication/SlashMainApplicationTests.java
@@ -0,0 +1,13 @@
+package site.lanmushan.mainapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlashMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
