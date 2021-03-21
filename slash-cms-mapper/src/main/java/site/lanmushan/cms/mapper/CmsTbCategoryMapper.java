e4cb9c1eddcdc0038ce69dce67127c5bf24ab13c
大熊猫 <lanmu_shan@163.com>
2021-03-21 23:14:40 +0800

    更新打包问题

--- slash-cms-mapper/src/main/java/site/lanmushan/cms/mapper/CmsTbCategoryMapper.java
+++ slash-cms-mapper/src/main/java/site/lanmushan/cms/mapper/CmsTbCategoryMapper.java
@@ -1,18 +1,10 @@
 package site.lanmushan.cms.mapper;
 
-
+import org.apache.ibatis.annotations.Mapper;
 import site.lanmushan.cms.api.entity.CmsTbCategory;
 import site.lanmushan.framework.query.mapper.QueryMapper;
 
-
-/**
- * (CmsTbCategory)表数据库访问层
- *
- * @author dy
- * @since 2021-01-30 14:54:16
- */
-@org.apache.ibatis.annotations.Mapper
-public interface CmsTbCategoryMapper extends QueryMapper<CmsTbCategory> {
-
+@Mapper
+public interface CmsTbCategoryMapper  extends QueryMapper<CmsTbCategory> {
 
 }
--- slash-main-application/src/main/java/site/lanmushan/starter/SlashMainApplication.java
+++ slash-main-application/src/main/java/site/lanmushan/starter/SlashMainApplication.java
@@ -1,18 +1,11 @@
 package site.lanmushan.starter;
 
 import com.alibaba.cloud.seata.GlobalTransactionAutoConfiguration;
-import com.alibaba.cloud.seata.SeataProperties;
 import com.alibaba.cloud.seata.web.SeataHandlerInterceptorConfiguration;
-import org.apache.logging.log4j.ThreadContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import site.lanmushan.framework.dto.Message;
 import tk.mybatis.spring.annotation.MapperScan;
 
 import java.util.ResourceBundle;
@@ -23,7 +16,7 @@ import java.util.ResourceBundle;
  * @author Administrator
  */
 @SpringBootApplication(scanBasePackages = "site.lanmushan", exclude = {SeataHandlerInterceptorConfiguration.class, GlobalTransactionAutoConfiguration.class})
-@MapperScan(value = {"site.lanmushan.*.mapper","site.lanmushan.framework.query.mapper"})
+@MapperScan(value = {"site.lanmushan.*.mapper", "site.lanmushan.framework.query.mapper"})
 @Controller
 @ServletComponentScan
 public class SlashMainApplication {
